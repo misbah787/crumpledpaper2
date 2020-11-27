8be7ef40b6d77b7bb907eb01b357d182ad2afbdd
misbah787 <73197552+misbah787@users.noreply.github.com>
2020-11-28 01:35:49 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,32 @@
+class Ground
+{constructor(x,y,width,height)
+    {
+        var options=
+        {
+        isStatic:true,
+        friction:0.5,
+        density:1.2
+        }
+     
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.body=Bodies.rectangle(this.x,this.y,this.width,this.height,options);
+        World.add(world, this.body);
+    }
+    display(){
+        strokeWeight(4);
+    stroke("red");
+fill("red");
+rect(this.x,this.y,this.width,this.height)
+    }
+    
+}
+
+    
+
+
+    
+
+
